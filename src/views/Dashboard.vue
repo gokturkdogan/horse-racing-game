5d3430f74129a4d2b45aeac81236ea7c2429410f
göktürk DOĞAN <gokturk.dogan@hotmail.com>
2025-02-18 12:01:53 +0300

    horse-list created by mock data

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,17 +1,20 @@
 <template>
   <Layout>
-    
+    <div class="dashboard">
+        <HorseList />
+    </div>
   </Layout>
 </template>
   
 <script>
 import Layout from '../layout/Main.vue';
+import HorseList from '../components/dashboard/HorseList.vue';
 
 export default {
   name: "Dashboard",
   components: {
-    Layout
+    Layout,
+    HorseList
   }
 };
 </script>
-  
