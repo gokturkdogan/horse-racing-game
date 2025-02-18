b7eb632937a46106db4aa29f9fd6c225334db167
göktürk DOĞAN <gokturk.dogan@hotmail.com>
2025-02-18 11:05:32 +0300

    header design created scss installed

--- src/layout/Main.vue
+++ src/layout/Main.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="layout">
+    <Header />
     <slot></slot>
   </div>
 </template>
   
 <script>
+import Header from '../components/base/Header.vue';
 export default {
   name: "MainLayout",
+  components: {
+    Header
+  }
 };
 </script>
   
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @use '@/assets/scss/variables/colors' as *;
+        @use '@/assets/scss/mixins/media-queries' as *;
+        @use '@/assets/scss/variables/font' as *;
+        `
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
